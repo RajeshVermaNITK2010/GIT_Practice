068d616dfc9154d296fe53291df5c8be3cfa305c
RajeshVermaNITK2010 <44517298+RajeshVermaNITK2010@users.noreply.github.com>
2024-05-12 21:29:47 +0530

    Code enhancement

--- Program.cs
+++ Program.cs
@@ -7,4 +7,5 @@ var sum = b1 + b2;
 Console.WriteLine(sum);
 
 Employee user1 = new Manager();
+Console.WriteLine("${0}: {1} :", user1.Bonus, user1.GetType().Name);
 Console.WriteLine("${0}: {1} : {2}", user1.Bonus, user1.GetType().Name, user1.GetType());
